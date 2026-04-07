da481135a81bedd8cabe644b10dea16b1f13789f
Nullora <nullora@tutamail.com>
2026-04-07 17:07:45 +0100

    added same thing for linux

--- src/spice_lin.cpp
+++ src/spice_lin.cpp
@@ -20,6 +20,8 @@ int main(int argc, char* argv[]){
     app_indicator_set_menu(indicator, GTK_MENU(menu));
     system("notify-send 'NSpice' 'Running first check...'");
     addToStartup();
+    exec("spicetify update");
+    exec("spicetify apply");
     thread checker([](){
         string version;
         while(true){
